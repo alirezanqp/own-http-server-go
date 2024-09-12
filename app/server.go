d830ecca9a1bfe0d326fa8c8dcbd67340936d4b8
Alireza Naghdipour <alirezanqp@gmail.com>
2024-09-12 20:10:52 +0330

    feat: add 200 response

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -10,17 +11,43 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+		// Block until we receive an incoming connection
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection
+		handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	defer conn.Close()
+
+	// Read data
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return
+	}
+
+	log.Println("Received data", buf[:n])
+
+	// Write the same data back
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
